5a337b571a6ae4a5b1e863ed998949456060c372
Andres Hoyos <andresfabianhoyos@gmail.com>
2022-09-10 17:56:42 -0300

    axios

--- public/app.js
+++ public/app.js
@@ -153,54 +153,55 @@ send.addEventListener("click", () => {
     const loader = document.querySelector(".swal2-loader");
     loader.style =
       "width: 80px; height: 80px; border: 6px solid; border-color: #2778c4 transparent #2778c4 transparent;";
-    // axios
-    //   .post("/mail", {
-    //     body: JSON.stringify({
-    //       firstName: firstName.value,
-    //       lastName: lastName.value,
-    //       email: email.value,
-    //       msg: msg.value,
-    //     }),
-    //   })
-    //   .then(function (res) {
-    //     console.log(res.data);
-    //     Swal.fire({
-    //       title: res.data,
-    //       padding: "3em",
-    //       color: "#560bad",
-    //     });
-    //     firstName.value = "";
-    //     lastName.value = "";
-    //     email.value = "";
-    //     msg.value = "";
-    //   })
-    //   .catch(function (error) {
-    //     console.log(error);
-    //   });
-
-    fetch("/mail", {
-      method: "POST",
-      headers: new Headers({
-        "Content-type": "application/json",
-        Accept: "application/json",
-      }),
-      body: JSON.stringify({
+    axios({
+      method: "post",
+      url: "/mail",
+      data: {
         firstName: firstName.value,
         email: email.value,
         msg: msg.value,
-      }),
+        // body: JSON.stringify({
+        // }),
+      },
     })
-      .then((res) => res.json())
-      .then((data) => {
+      .then(function (res) {
+        console.log(res.data);
         Swal.fire({
-          title: data,
+          title: res.data,
           padding: "3em",
           color: "#560bad",
         });
         firstName.value = "";
         email.value = "";
         msg.value = "";
+      })
+      .catch(function (error) {
+        console.log(error);
       });
+
+    // fetch("/mail", {
+    //   method: "POST",
+    //   headers: new Headers({
+    //     "Content-type": "application/json",
+    //     Accept: "application/json",
+    //   }),
+    //   body: JSON.stringify({
+    //     firstName: firstName.value,
+    //     email: email.value,
+    //     msg: msg.value,
+    //   }),
+    // })
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     Swal.fire({
+    //       title: data,
+    //       padding: "3em",
+    //       color: "#560bad",
+    //     });
+    //     firstName.value = "";
+    //     email.value = "";
+    //     msg.value = "";
+    //   });
   }
 });
 
